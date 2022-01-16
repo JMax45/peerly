53b1e0035c9da679cb87bf97035896f651f0c26a
JMax45 <maxmailru.45@gmail.com>
2022-01-16 17:14:21 +0100

    fix: add getServerList to exported utils

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import Client from './Client';
 import SignalingServer from './SignalingServer';
 import Controller from './Client/Controller';
+import getServerList from './SignalingServer/getServerList';
 
 export { Client, SignalingServer, Controller };
+export const utils = {
+	getServerList,
+};
